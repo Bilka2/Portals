e27b06a7b61e1c47c9f772308f2ba0e1c1082e1c
Bilka <Bilka2@users.noreply.github.com>
2017-12-08 23:52:20 +0100

    Removed unused entities

--- prototypes/entities/portals.lua
+++ prototypes/entities/portals.lua
@@ -98,6 +98,7 @@ data:extend(
 		tile_width = 1,
 		tile_height = 2,
 	},
+	
 	 --just a renamed flying-text so that when my mod gets removed the text also gets removed
 	{
 		type = "flying-text",
@@ -105,98 +106,5 @@ data:extend(
 		flags = {"not-on-map", "placeable-off-grid"},
 		time_to_live = 150,
 		speed = 0.05
-	},
-	
-	{
-		type = "lab", --old, here for migration
-		name = "portal-base-a",
-		flags = {"not-blueprintable", "not-deconstructable"},
-		friendly_map_color = {r=1, g=1, b=0},
-		selectable_in_game = true,
-		minable = {mining_time = 0.7, result = nil},
-		max_health = 20,
-		collision_box = {{0, -0.6}, {0, 0.6}},
-		collision_mask = { "item-layer", "object-layer", "water-tile"},
-		selection_box = {{-0.5, -0.9}, {0.5, 0.9}},
-		energy_source= 
-		{
-			type = "electric",
-			effectivity = 1,
-			usage_priority = "primary-input",
-			render_no_power_icon = false,
-			render_no_network_icon = false,
-		},
-		off_animation =
-		{
-			filename = "__Portals__/graphics/null.png",
-			priority = "high",
-			width = 1,
-			height = 1,
-			frame_count = 1,
-			line_length = 1,
-			animation_speed = 1,
-		},
-		on_animation =
-		{
-			filename = "__Portals__/graphics/null.png",
-			priority = "high",
-			width = 1,
-			height = 1,
-			frame_count = 1,
-			line_length  = 1,
-			animation_speed = 1,
-		},
-		energy_usage = "0kW",
-		inputs = {},
-		module_slots = {},
-		tile_width = 1,
-		tile_height = 2,
-	},
-	
-	{
-		type = "lab",
-		name = "portal-base-b", --old, here for migration
-		flags = {"not-blueprintable", "not-deconstructable"},
-		friendly_map_color = {r=1, g=1, b=0},
-		selectable_in_game = true,
-		minable = {mining_time = 0.7, result = nil},
-		max_health = 20,
-		collision_box = {{0, -0.6}, {0, 0.6}},
-		collision_mask = { "item-layer", "object-layer", "water-tile"},
-		selection_box = {{-0.5, -0.9}, {0.5, 0.9}},
-		energy_source= 
-		{
-			type = "electric",
-			effectivity = 1,
-			usage_priority = "primary-input",
-			render_no_power_icon = false,
-			render_no_network_icon = false,
-		},
-		off_animation =
-		{
-			filename = "__Portals__/graphics/null.png",
-			priority = "high",
-			width = 1,
-			height = 1,
-			frame_count = 1,
-			line_length  = 1,
-			animation_speed = 1,
-		},
-		on_animation =
-		{
-			filename = "__Portals__/graphics/null.png",
-			priority = "high",
-			width = 1,
-			height = 1,
-			frame_count = 1,
-			line_length  = 1,
-			animation_speed = 1,
-		},
-		energy_usage = "0kW",
-		inputs = {},
-		module_slots = {},
-		tile_width = 1,
-		tile_height = 2,
-	},
-	
+	}
 })
